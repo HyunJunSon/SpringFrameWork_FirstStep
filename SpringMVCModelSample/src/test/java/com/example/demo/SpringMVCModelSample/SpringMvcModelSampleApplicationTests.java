dbf3b69197a5bd446b64877df7a4fe7cae70c674
HyunJunSon <jjang3a@gmail.com>
2023-08-26 16:26:52 +0900

    thymleaf chapter06

---
+++ SpringMVCModelSample/src/test/java/com/example/demo/SpringMVCModelSample/SpringMvcModelSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.SpringMVCModelSample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcModelSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
