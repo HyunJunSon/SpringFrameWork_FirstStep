24af4ac6b20b9acc89c6b8be41288de6c31e74be
HyunJunSon <jjang3a@gmail.com>
2023-08-28 17:24:22 +0900

    tymeleaf study

--- SpringMVCModelSample/src/main/java/com/example/demo/controller/HelloModelController.java
+++ SpringMVCModelSample/src/main/java/com/example/demo/controller/HelloModelController.java
@@ -5,6 +5,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+import java.util.Map;
+
 @Controller
 @RequestMapping("hello")
 public class HelloModelController {
@@ -12,6 +15,16 @@ public class HelloModelController {
     @GetMapping("model")
     public String helloView(Model model) {
         model.addAttribute("msg", "타임리프!!");
-        return "helloThymeleaf";
+        model.addAttribute("mb", new MB(1234,"Son"));
+        model.addAttribute("list", List.of("동","서","남","북"));
+        model.addAttribute("map", Map.of("kim",new MB(1234,"kimHyunJun"),"son",new MB(2345, "SonHyunJun")));
+        model.addAttribute("members",
+                List.of(new MB(1,"son"),
+                        new MB(2,"choi"),
+                        new MB(3,"koo"),
+                        new MB(4,"mun")
+                ));
+
+        return "Test";
     }
 }
---
+++ SpringMVCModelSample/src/main/java/com/example/demo/controller/MB.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+
+public class MB {
+    Integer ID;
+    String NAME;
+
+    public MB(Integer ID, String NAME) {
+        this.ID = ID;
+        this.NAME = NAME;
+    }
+
+    public Integer getID() {
+        return ID;
+    }
+
+    public String getNAME() {
+        return NAME;
+    }
+}
